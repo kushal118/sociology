1d91b23a219ebaa6d3db5afe48391533945e8677
kushal118 <irfankushal02@gmail.com>
2023-06-21 05:20:57 +0600

    basic configuration of the project

---
+++ server/.history/index_20230621050348.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan"
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS*/
+const __filename = fileURLToPath(import.meta)
---
+++ server/.history/index_20230621050846.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan"
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use
